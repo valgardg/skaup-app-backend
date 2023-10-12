fc15a1864bd9ff51d6860739c785c3a46c01c8fa
valgardg <valgardo20@ru.is>
2023-10-12 19:23:21 +0000

    initial commit for backend

---
+++ server.js
@@ -0,0 +1,130 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+const app = express();
+// Add this to allow CORS
+const allowedOrigins = [
+    "http://localhost:3000",
+    // "http://192.168.1.15:3000",
+];
+app.use(cors({
+    origin: function(origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    }
+  }));
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+    //   origin: "http://localhost:3000",  // Your React app's origin
+      origin: allowedOrigins,
+      methods: ["GET", "POST"],
+      credentials: true
+    }
+});
+
+class GameState {
+    constructor() {
+        this.players = [];
+        this.phase = "GuessPhase";
+    }
+}
+
+class Player {
+    constructor(socketId, name, color) {
+        this.socketId = socketId;
+        this.name = name;
+        this.color = color;
+        this.guesses = [];
+        this.vote_status = false;
+    }
+
+    addGuess(guess) {
+        this.guesses.push(guess);
+    }
+}
+
+var gameState = new GameState();
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+    console.log(socket.id);
+
+    socket.on('fetch-game', (data) => {
+        console.log("game state fetched");
+        io.emit('game-state', gameState);
+    });
+
+
+    socket.on('join-game', (data) => {
+        var newPlayer = new Player(socket.id, data.name, data.color);
+        gameState.players.push(newPlayer);
+        io.emit('game-state', gameState);
+    });
+
+    socket.on('submit-guess', (data) => {
+        if(gameState.phase === "WatchPhase" || gameState.players.find(player => player.name === data.name).vote_status === true) {
+            return;
+        }
+        var player = gameState.players.find(player => player.name === data.name);
+        player.addGuess(data.guess);
+        console.log('Received guess from client: ', data);
+        io.emit('game-state', gameState);
+    });
+
+    socket.on('delete-guess', (data) => {
+        if(gameState.phase === "WatchPhase" || gameState.players.find(player => player.name === data.name).vote_status === true) {
+            return;
+        }
+        var player = gameState.players.find(player => player.name === data.name);
+        player.guesses = player.guesses.filter(guess => guess !== data.guess);
+        console.log('Removing guess from client: ', data);
+        io.emit('game-state', gameState);
+    });
+
+    socket.on("player-ready", (data) => {
+        if(gameState.phase === "WatchPhase") {
+            return;
+        }
+        var player = gameState.players.find(player => player.name === data.name);
+        player.vote_status = true;
+        if(gameState.players.every(player => player.vote_status === true)) {
+            gameState.phase = "WatchPhase";
+        }
+        io.emit('game-state', gameState);
+    });
+
+    socket.on("player-unready", (data) => {
+        if(gameState.phase === "WatchPhase") {
+            return;
+        }
+        var player = gameState.players.find(player => player.name === data.name);
+        player.vote_status = false;
+        io.emit('game-state', gameState);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+        console.log(socket.id);
+        gameState.players = gameState.players.filter(player => player.socketId !== socket.id);
+        io.emit("game-state", gameState);
+    });
+
+    socket.on('reset-game', () => {
+        console.log('Resetting game');
+        gameState = new GameState();
+        io.emit('game-state', gameState);
+    });
+});
+
+// server.listen(3001, '0.0.0.0', () => { // used for local network testing
+server.listen(3001, () => {
+  console.log('Server listening on port 3001');
+});
